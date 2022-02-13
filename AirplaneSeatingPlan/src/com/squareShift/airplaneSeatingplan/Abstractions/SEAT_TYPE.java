9666ea06da931de49ebcc9dcf603a70f314d8c68
manish797 <manish.gupta797@gmail.com>
2022-02-14 01:36:27 +0530

    Update SEAT_TYPE.java

--- AirplaneSeatingPlan/src/com/squareShift/airplaneSeatingplan/Abstractions/SEAT_TYPE.java
+++ AirplaneSeatingPlan/src/com/squareShift/airplaneSeatingplan/Abstractions/SEAT_TYPE.java
@@ -1,5 +1,12 @@
 package com.squareShift.airplaneSeatingplan.Abstractions;
 
+/*
+ * This is the enum for the Seat type ,
+ * W for Window
+ * A for Aisle
+ * M for Middle.
+ * Using the shorter notation of each of code representation.
+ */
 public enum SEAT_TYPE {
 
 	W, A, M
